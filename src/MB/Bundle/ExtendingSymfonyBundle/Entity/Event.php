26927da67a27cde3526b59c771e3b3a5b7d21ad2
Marek <blazek.mario@gmail.com>
2015-06-02 08:53:17 +0200

    User and Event entities added

---
+++ src/MB/Bundle/ExtendingSymfonyBundle/Entity/Event.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace MB\Bundle\ExtendingSymfonyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="mb_events")
+ * @ORM\Entity
+ */
+class Event
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="latitude", type="float")
+     */
+    private $latitude;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="longitude", type="float")
+     */
+    private $longitude;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="User", inversedBy="events")
+	 */
+	private $attendees;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->attendees = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Event
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     * @return Event
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     * @return Event
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+
+    /**
+     * Add attendees
+     *
+     * @param User $attendees
+     * @return Event
+     */
+    public function addAttendee(User $attendees)
+    {
+        $this->attendees[] = $attendees;
+
+        return $this;
+    }
+
+    /**
+     * Remove attendees
+     *
+     * @param User $attendees
+     */
+    public function removeAttendee(User $attendees)
+    {
+        $this->attendees->removeElement($attendees);
+    }
+
+    /**
+     * Get attendees
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getAttendees()
+    {
+        return $this->attendees;
+    }
+}
---
+++ src/MB/Bundle/ExtendingSymfonyBundle/Entity/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace MB\Bundle\ExtendingSymfonyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="mb_user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="Event", mappedBy="attendees")
+	 */
+	private $events;
+
+	/**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add events
+     *
+     * @param Event $events
+     * @return User
+     */
+    public function addEvent(Event $events)
+    {
+        $this->events[] = $events;
+
+        return $this;
+    }
+
+    /**
+     * Remove events
+     *
+     * @param Event $events
+     */
+    public function removeEvent(Event $events)
+    {
+        $this->events->removeElement($events);
+    }
+
+    /**
+     * Get events
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getEvents()
+    {
+        return $this->events;
+    }
+}
